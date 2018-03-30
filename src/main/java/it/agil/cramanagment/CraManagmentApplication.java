d0d5d0bd8129d6869bc6b407c15c771553714de6
sanjiken <touibi.nader@gmail.com>
2018-03-30 12:27:20 +0200

    first commit

---
+++ src/main/java/it/agil/cramanagment/CraManagmentApplication.java
@@ -0,0 +1,12 @@
+package it.agil.cramanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraManagmentApplication.class, args);
+	}
+}
---
+++ src/main/java/it/agil/cramanagment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.agil.cramanagment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CraManagmentApplication.class);
+	}
+
+}
